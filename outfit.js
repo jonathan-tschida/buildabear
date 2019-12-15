c5fbea40a24f30d5d985a9ba2bf02690cf6b00b3
Erik Raith <raithhtiar@gmail.com>
2019-12-15 12:21:27 -0700

    Initial frameing laidout for outfit class

---
+++ outfit.js
@@ -0,0 +1,15 @@
+class Outfit{
+  constructor(id,title,garment,background){
+    this.id = id;
+    this.title = 'title';
+    this.garment = ['Hat','Clothes','Accessories'];
+    this.background = background;
+  }
+  addGarmet(garmentName){
+    this.garmen[i]=garmentName;
+  }
+  removeGarmet(garmetName){
+
+  }
+}
+module.exports = Outfit;
