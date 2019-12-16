2c0956330eb1d116f99db11964721427d996ef37
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-16 12:31:44 -0700

    Remove comments and add if statement to fix bug

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 var backgroundButtons = document.getElementById("background-buttons");
 var bearBox = document.getElementById("bear-container");
 
-// store default background in local storage
 window.localStorage.setItem('currentBackground', 'blue')
-// use event bubbling on background button container
 backgroundButtons.addEventListener('click', changeBackground);
 
 function changeBackground() {
-  // button text and convert to lower case
-  var background = event.target.innerText.toLowerCase();
-  // get current background to delete the bear box class
-  // and change the currentBackground in local storage
-  var currentBackground = window.localStorage.getItem('currentBackground');
-  window.localStorage.setItem('currentBackground', background)
-  bearBox.classList.remove(currentBackground);
-  // add new background that was stored in button text
-  bearBox.classList.add(background);
+  if (event.target.classList.contains('item-button')) {
+    var background = event.target.innerText.toLowerCase();
+    var currentBackground = window.localStorage.getItem('currentBackground');
+    window.localStorage.setItem('currentBackground', background)
+    bearBox.classList.remove(currentBackground);
+    bearBox.classList.add(background);
+  }
 }
