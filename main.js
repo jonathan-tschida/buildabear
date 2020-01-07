de92e4e3dfaa86f5e5ea59ebcc5b554b1b2086f0
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-06 20:22:53 -0700

    Refactored a little

--- main.js
+++ main.js
@@ -155,12 +155,12 @@ function removeCard(event) {
 
 function loadSavedOutfit(id){
   clearBearDisplay();
-  for(var i =0; i<outfits.length; i++){
+  for(var i = 0; i < outfits.length; i++){
     if(outfits[i].id == id){
       Object.assign(currentOutfit,outfits[i]);
-      (outfits[i].garments[0]!=null)?showGarmentOnBear(outfits[i].garments[0].replace(/\s+/g, '-').toLowerCase(), 'hat-container') : '';
-      (outfits[i].garments[1]!=null)?showGarmentOnBear(outfits[i].garments[1].replace(/\s+/g, '-').toLowerCase(), 'clothing-container') : '';
-      (outfits[i].garments[2]!=null)?showGarmentOnBear(outfits[i].garments[2].replace(/\s+/g, '-').toLowerCase(), 'accessory-container') : '';
+      (outfits[i].garments[0]!= null) ? showGarmentOnBear(outfits[i].garments[0].replace(/\s+/g, '-').toLowerCase(), 'hat-container') : '';
+      (outfits[i].garments[1]!= null) ? showGarmentOnBear(outfits[i].garments[1].replace(/\s+/g, '-').toLowerCase(), 'clothing-container') : '';
+      (outfits[i].garments[2]!= null) ? showGarmentOnBear(outfits[i].garments[2].replace(/\s+/g, '-').toLowerCase(), 'accessory-container') : '';
       bearBox.classList.add(outfits[i].background);
       titleInput.value = outfits[i].title;
       enableSaveButton();
